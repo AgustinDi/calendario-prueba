32f4a32e70ad67cbc0be32fb86ea7bf98a4a39e7
AgustinDi <agustindigiacinto18@gmail.com>
2023-04-28 10:25:49 -0300

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Prueba Monday - Calendario dinamico</h1>
+      <p>Probando la api de <a href="https://monday.com/lang/es" target='_blank'>monday</a></p>
+    </>
+  )
+}
+
+export default App
